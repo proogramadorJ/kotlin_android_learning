4ca7efda0a7de271cb4564940b30e3a06dd7be71
Pedro <pedroenxadrista1@gmail.com>
2025-04-19 21:16:36 -0300

    update in-mememory to-do-list app

--- InMemoryTodoList/app/src/main/java/com/pedrodev/inmemorytodolist/MainActivity.kt
+++ InMemoryTodoList/app/src/main/java/com/pedrodev/inmemorytodolist/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.pedrodev.inmemorytodolist
 
+import android.R
 import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,15 +12,20 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
 import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -32,6 +38,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import com.pedrodev.inmemorytodolist.model.ItemTodo
 import com.pedrodev.inmemorytodolist.ui.theme.InMemoryTodoListTheme
 import com.pedrodev.inmemorytodolist.util.DateUtil
@@ -79,17 +86,20 @@ fun MainContent(items: List<ItemTodo>) {
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
-fun MainListComposable(notes: List<ItemTodo>) {
+fun MainListComposable(itemTodos: List<ItemTodo>) {
+
+    var clickedItem: ItemTodo? by remember { mutableStateOf(null) }
+
     LazyColumn(
-        modifier = Modifier
-            .fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth(),
         verticalArrangement = Arrangement.spacedBy(8.dp),
     ) {
-        items(notes) { note ->
-            ItemComposable(note) { println("Clicked on " + note.title) }
+        items(itemTodos) { item ->
+            ItemComposable(item) { clickedItem = item }
             HorizontalDivider()
         }
     }
+    ItemDetailDialog(clickedItem, { clickedItem = null }, {}, {})
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
@@ -101,8 +111,7 @@ fun ItemComposable(item: ItemTodo, clickOnItem: (note: ItemTodo) -> Unit) {
     val textButton = if (item.isDone) "Completed" else "Pedding"
 
     Box(
-        modifier = Modifier.clickable { clickOnItem(item) }
-    ) {
+        modifier = Modifier.clickable { clickOnItem(item) }) {
         Row {
             Column {
                 Text(item.title, style = TextStyle(fontSize = 14.sp, fontWeight = FontWeight.Bold))
@@ -111,8 +120,7 @@ fun ItemComposable(item: ItemTodo, clickOnItem: (note: ItemTodo) -> Unit) {
 
             Spacer(modifier = Modifier.weight(1f))
             Button(
-                onClick = {},
-                colors = ButtonDefaults.buttonColors(
+                onClick = {}, colors = ButtonDefaults.buttonColors(
                     containerColor = backGroundButtonColor
                 )
             ) {
@@ -123,21 +131,61 @@ fun ItemComposable(item: ItemTodo, clickOnItem: (note: ItemTodo) -> Unit) {
 }
 
 @Composable
-fun ShowItemDialog() {
-    Text("Hello")
+fun ItemDetailDialog(
+    item: ItemTodo?,
+    closeDialog: () -> Unit,
+    saveItem: (item: ItemTodo) -> Unit,
+    deleteItem: () -> Unit
+) {
+    var title by remember { mutableStateOf("") }
+    var description by remember { mutableStateOf("") }
+
+    if (item != null) Dialog(onDismissRequest = closeDialog) {
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxSize()
+                .padding(top = 16.dp, bottom = 100.dp),
+            shape = RoundedCornerShape(16.dp)
+        ) {
+
+            Column(
+                Modifier.padding(16.dp)
+            ) {
+                OutlinedTextField(
+                    value = title,
+                    onValueChange = { title = it },
+                    label = { Text("Title") },
+                    shape = RoundedCornerShape(24.dp),
+                    maxLines = 1
+                )
+
+                OutlinedTextField(
+                    value = title,
+                    onValueChange = { title = it },
+                    label = { Text("Detail") },
+                    shape = RoundedCornerShape(24.dp),
+                    modifier = Modifier.height(400.dp).padding(top = 16.dp),
+                    maxLines = 10
+                )
+            }
+
+        }
+    }
 }
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview
 @Composable
 fun ShowItemDialogPreview() {
-    ShowItemDialog()
+    ItemDetailDialog(ItemTodo(1, "Item 1", "Item 1 Detail ", LocalDate.now(), false), {}, {}, {})
 }
 
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainContentPreview() {
 
     var items by remember {
         mutableStateOf(
